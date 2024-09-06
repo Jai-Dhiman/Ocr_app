6e7405ae10f0f81e82df9178be7a4e708a2e895b
JaiDhiman <jaidhiman2000@gmail.com>
2024-09-05 18:51:42 -0700

    added image url upload

--- app/controllers/ocr_controller.rb
+++ app/controllers/ocr_controller.rb
@@ -2,13 +2,19 @@ class OcrController < ApplicationController
   protect_from_forgery with: :null_session
 
   def create
-    uploaded_file = params[:image]
-    if uploaded_file
-      response = ocr_space_request(uploaded_file)
+    image_param = params[:image]
+
+    if image_param.present?
+      if image_param =~ URI::DEFAULT_PARSER.make_regexp
+        response = ocr_space_request_from_url(image_param)
+      else
+        response = ocr_space_request_from_file(image_param)
+      end
+
       @text = parse_ocr_response(response)
       render :show
     else
-      render json: { error: "Please upload a File" }, status: :bad_request
+      render json: { error: "Please upload a File or image URL" }, status: :bad_request
     end
   end
 
@@ -25,6 +31,19 @@ class OcrController < ApplicationController
       files: {
         "file" => file
       }
+    )
+    response.parsed_response
+  end
+
+  def ocr_space_request_from_url(url)
+    api_key = K82778956688957
+    response = HTTParty.post(
+      "https://api.ocr.space/parse/image",
+      body: {
+        apikey: api_key,
+        isOverlayRequired: false,
+        url: url
+      },
     )
     response.parsed_response
   end
